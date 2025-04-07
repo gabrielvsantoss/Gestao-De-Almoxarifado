93aa3edba36114293f23cb2dd7bd960dd03cc89d
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-04-07 20:01:55 -0300

    Add project files.

---
+++ GestaoDeAlmoxarifado.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GestaoDeAlmoxarifado.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
